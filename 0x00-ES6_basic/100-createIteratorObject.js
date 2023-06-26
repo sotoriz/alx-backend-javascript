757bf9d69e7df545bad7a822c417a255c85f28aa
sotoriz <sotorisfx@gmail.com>
2023-06-26 16:48:20 -0700

    Iterating through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,19 @@
 export default function createIteratorObject(report) {
-  while (report) {
-    console.log('Placeholder');
-  }
+  const all = Object.values(report.allEmployees).reduce((a, b) => {
+    a.push(...b);
+    return a;
+  }, []);
+  let currIndex = 0;
+  const maxIndex = all.length;
+  return {
+    next() {
+      if (currIndex < maxIndex) {
+        const result = { value: all[currIndex], done: false };
+        currIndex += 1;
+        return result;
+      }
+      return { value: null, done: true };
+    },
+    [Symbol.iterator]: () => this.next(),
+  };
 }
