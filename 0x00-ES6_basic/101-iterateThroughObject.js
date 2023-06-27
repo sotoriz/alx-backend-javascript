cdb8f95b30a7e570099c8004d3685c9da53c062d
sotoriz <sotorisfx@gmail.com>
2023-06-27 12:31:05 -0700

    Iterate through object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,5 +1,15 @@
-export default function iterateThroughObject(reportWithIterator) {
-  if (!reportWithIterator) {
+export default function iterateThroughObject(reportWithIterator);
+import createEmployeesObject from "./11-createEmployeesObject.js";
+import createReportObject from './12-createReportObject.js';
+import createIteratorObject from './100-createIteratorObject.js';
+import iterateThroughObject from './101-iterateThroughObject.js';
+
+
+const employees = {
+      ...createEmployeesObject('engineering', ['placeholder']),
+      ...createEmployeesObject('design', ['placeholder']),
+    };
+
+const report = createReportObject(employees);
+const reportWithIterator = createIteratorObject(report);
     console.log('Placeholder');
-  }
-}
