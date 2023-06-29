d6c66e46ed8583e3d21e109d5f8018271b6cfaa8
sotoriz <sotorisfx@gmail.com>
2023-06-29 15:13:49 -0700

    Iterator

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,3 @@
-export default function iterateThroughObject(reportWithIterator) {
-  if (!reportWithIterator) {
-    console.log('Placeholder');
-  }
+export default function createEmployeesObject(departmentName, employees) {
+  return { [departmentName]: [...employees] };
 }
