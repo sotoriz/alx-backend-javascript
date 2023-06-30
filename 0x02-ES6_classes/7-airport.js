9b5ea2f2bae0f0fd3d0e9bfa9c62f6952990e00f
sotoriz <sotorisfx@gmail.com>
2023-06-29 18:12:31 -0700

    Airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,19 +1,26 @@
 export default class Airport {
   constructor(name, code) {
-    // Create objs
-    this._code = code;
-    this._name = name;
+    this.name = name;
+    this.code = code;
   }
 
-  // Methods
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
 
   get [Symbol.toStringTag]() {
-    return `${this._code}`;
+    return this._code;
   }
-
-  // Setters
-
-
-  // Getter
-
 }
