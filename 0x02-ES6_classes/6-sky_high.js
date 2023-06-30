8b4aacf1f8ef3a820ae5cfc45343a2c371c702ce
sotoriz <sotorisfx@gmail.com>
2023-06-29 18:15:32 -0700

    Inheritance

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,29 +2,19 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    // call constructor of super class (Building)
     super(sqft);
-
-    // Create objs
-    this._floors = floors;
+    this.floors = floors;
   }
 
-  // Methods
-
-  evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors`;
+  get floors() {
+    return this._floors;
   }
-
-  // Setters
 
   set floors(value) {
     this._floors = value;
   }
 
-
-  // Getters
-
-  get floors() {
-    return this._floors;
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
